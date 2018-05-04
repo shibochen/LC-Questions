772224eae5e8bf5ab29dcf6faaa08d401e41d027
Shibo Chen <shibochenny@gmail.com>
2018-05-03 23:56:18 -0400

    5/3/18

--- LeetCode_Solutions/021. Merge_Two_Sorted_Lists/Merge_Two_Sorted_Lists_I.java
+++ LeetCode_Solutions/021. Merge_Two_Sorted_Lists/Merge_Two_Sorted_Lists_I.java
@@ -11,7 +11,7 @@
             //create a new node
             ListNode result = new ListNode(0);
             ListNode current = result;
-            
+
             //check if two list are null or not
             while(l1 != null && l2 != null){
                 //compare value of two lists
@@ -26,7 +26,12 @@
             }
             //if l1 == null, point to l2; otherwise, point to l1
             current.next = l1 == null ? l2 : l1;
-            
+
             return result.next;
         }
     }
+    /*
+    一般情况可以建立虚node来解决问题
+
+    Time: O(n) space: O(1);
+    */
