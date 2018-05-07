c39f39bf2bb9692d72d0ec931bef5557839cdd69
Shibo Chen <38870192+shibochen@users.noreply.github.com>
2018-05-07 00:21:59 -0400

    Update Range_Sum_Query_Immutable_I.java

--- LeetCode_Solutions/303. Range_Sum_Query_Immutable/Range_Sum_Query_Immutable_I.java
+++ LeetCode_Solutions/303. Range_Sum_Query_Immutable/Range_Sum_Query_Immutable_I.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class NumArray {
     private int[] data;
 
@@ -25,31 +24,3 @@ class NumArray {
  /*
  Method: Brutal Force
  */
-=======
-class NumArray {
-    private int[] data;
-
-    public NumArray(int[] nums) {
-        data = nums;
-    }
-
-    public int sumRange(int i, int j) {
-        int sum = 0;
-        for(int k = i; k <= j; k++){
-            sum += data[k];
-        }
-        return sum;
-    }
-}
-
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(i,j);
- */
-
-
- /*
- Method: Brutal Force
- */
->>>>>>> 59ec0388aed1323bba68cd071f3bc1e7cff917c3
