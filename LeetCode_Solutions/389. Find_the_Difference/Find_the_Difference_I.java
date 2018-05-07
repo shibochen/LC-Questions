cf12a2046abd593bdb1066f3c3ef40671519e648
Shibo Chen <38870192+shibochen@users.noreply.github.com>
2018-05-07 00:21:28 -0400

    Update Find_the_Difference_I.java

--- LeetCode_Solutions/389. Find_the_Difference/Find_the_Difference_I.java
+++ LeetCode_Solutions/389. Find_the_Difference/Find_the_Difference_I.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution {
     public char findTheDifference(String s, String t) {
         char letter = 0;
@@ -18,24 +17,3 @@ Method: Bit Manipulation
 
 Time: O(t)  Space: O(1)
 */
-=======
-class Solution {
-    public char findTheDifference(String s, String t) {
-        char letter = 0;
-        for(char c : s.toCharArray()){
-             letter ^= c;
-        }
-        for(char c: t.toCharArray()){
-             letter ^= c;
-        }
-        return letter;
-    }
-}
-
-
-/*
-Method: Bit Manipulation
-
-Time: O(t)  Space: O(1)
-*/
->>>>>>> 59ec0388aed1323bba68cd071f3bc1e7cff917c3
