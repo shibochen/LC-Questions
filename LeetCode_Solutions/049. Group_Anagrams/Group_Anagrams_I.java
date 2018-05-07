25705bb5c20dfcd3e6d1301172c432b0ea8d5ddb
Shibo Chen <38870192+shibochen@users.noreply.github.com>
2018-05-07 00:20:07 -0400

    Update Group_Anagrams_I.java

--- LeetCode_Solutions/049. Group_Anagrams/Group_Anagrams_I.java
+++ LeetCode_Solutions/049. Group_Anagrams/Group_Anagrams_I.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         //create an empty arraylist
@@ -39,45 +38,4 @@ class Solution {
 思路：将array里的string重新sort再判定element是否是相同，相同就放在每组sublist里，然后在把所有sublist
     放到一个总List里面
 */
-=======
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        //create an empty arraylist
-        List<List<String>> result = new ArrayList<List<String>>();
-        //boundary check
-        if(strs == null || strs.length == 0) return result;
-        //create an emtpy hashmap
-        HashMap<String, Integer> map = new HashMap<>();
-        //iterate the array, strs[]
-        for(String str : strs){
-            //convert each element of array,strs[] to array
-            char[] letter = str.toCharArray();
-            //sort each character
-            Arrays.sort(letter);
-            //convert array to string
-            String s = new String(letter);
-            //check whether map contains key
-            if(map.containsKey(s)){
-                //if it has, find the corresponding list
-                List<String> list = result.get(map.get(s));
-                //add element to the list
-                list.add(str);
-            }else{
-                //create an empty arraylist
-                List<String> list = new ArrayList<>();
-                //add element to the List
-                list.add(str);
-                //store element as key and size as value to the map;
-                map.put(s, result.size());
-                result.add(list);
-            }
-        }
-        return result;
-    }
-}
 
-/*
-思路：将array里的string重新sort再判定element是否是相同，相同就放在每组sublist里，然后在把所有sublist
-    放到一个总List里面
-*/
->>>>>>> 59ec0388aed1323bba68cd071f3bc1e7cff917c3
