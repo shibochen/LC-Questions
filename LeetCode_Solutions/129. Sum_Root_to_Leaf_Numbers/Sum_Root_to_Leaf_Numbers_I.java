64cc4168fc9749ee24bef960d2861593fd8a1585
Shibo Chen <shibochenny@gmail.com>
2018-05-29 20:07:50 -0400

    5/29/18

---
+++ LeetCode_Solutions/129. Sum_Root_to_Leaf_Numbers/Sum_Root_to_Leaf_Numbers_I.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+    private int dfs(TreeNode root, int prev) {
+        if(root == null) {
+            return 0;
+        }
+
+        int sum = root.val + prev * 10;
+
+        if(root.left == null && root.right == null) return sum;
+
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+}
