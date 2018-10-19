40736d72f1f70492912f59e4a189a0dc28ae684b
Shibo Chen <shibochenny@gmail.com>
2018-10-18 21:36:34 -0400

    10/18/2018

---
+++ Solutions/268/solution_I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int result = nums.length;
+        
+        for (int i = 0; i < nums.length; i++) {
+            result ^= (i ^ nums[i]);
+        }
+        
+        return result;
+    }
+}
+
+TC: O(n)
+SC: O(1)
+Method: Bit Manipulation
---
+++ Solutions/268/solution_II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int expectedSum = nums.length * (nums.length + 1 ) / 2;
+        int actualSum = 0;
+        
+        for (int num : nums) {
+            actualSum += num;
+        }
+        
+        return expectedSum - actualSum;
+    }
+}
+
+TC: O(n)
+SC: O(1)
+Method: Math
