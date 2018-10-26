9fbdcc31fbaa5e2a3ab62f264cda46dd724b5c6c
Shibo Chen <shibochenny@gmail.com>
2018-10-25 22:39:33 -0400

    10/25/18

---
+++ Solutions/338/solution_I.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int num) {
+        int[] result = new int[num + 1];
+        for (int i = 1; i <= num; i++) {
+            result[i] = result[i & (i - 1)] + 1;
+        }
+        return result;
+    }
+}
+
+TC: O(n)
+SC: O(n)
+M: Bit Manipulation
