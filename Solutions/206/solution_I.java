ce8b8bb7073248ab54ccc2e84391f33baaf25e77
Shibo Chen <shibochenny@gmail.com>
2018-11-06 22:34:57 -0500

    11/6/2018

---
+++ Solutions/206/solution_I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode pre = null;
+        
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+}
+
+TC: O(n)
+SC: O(1)
