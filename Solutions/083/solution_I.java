ee70952f8e6067454c22b70848eb6889905f5728
Shibo Chen <shibochenny@gmail.com>
2018-12-04 22:27:19 -0500

    12/4/18

--- Solutions/083/solution_I.java
+++ Solutions/083/solution_I.java
@@ -1,14 +1,18 @@
-public ListNode deleteDuplicates(ListNode head) {
-    ListNode current = head;
-    while (current != null && current.next != null) {
-        if (current.next.val == current.val) {
-            current.next = current.next.next;
-        } else {
-            current = current.next;
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        
+        ListNode current = head;
+        
+        while(current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
         }
+        return head;
     }
-    return head;
 }
-
 TC: O(N)
 SC: O(1)
