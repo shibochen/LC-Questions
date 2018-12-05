755fd8ca1659a773e0581aac3fde8cf82fe13bf4
Shibo Chen <shibochenny@gmail.com>
2018-12-04 22:51:48 -0500

    12/4/18

--- Solutions/141/solution_I.java
+++ Solutions/141/solution_I.java
@@ -1,19 +1,19 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if (head == null) return false;
+        if(head == null || head.next == null) return false;
         
         ListNode slow = head;
         ListNode fast = head;
         
-        while (fast.next != null && fast.next.next != null){
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            if (slow == fast) return true;
+            if(slow == fast) {
+                return true;
+            }
         }
-        
         return false;
     }
 }
-
 TC: O(n)
 SC: O(1)
