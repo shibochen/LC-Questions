a70ecda6b0ae85de188f4c12c855edd7abd351db
Shibo Chen <shibochenny@gmail.com>
2018-12-06 21:04:38 -0500

    12/6/18

---
+++ Solutions/203/solution_I.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return head;
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+        
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
+
+T:O(n);
+S:O(1);
