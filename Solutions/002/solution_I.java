57932cbdc7a523e46128a0053102498c0bf81bd0
Shibo Chen <shibochenny@gmail.com>
2018-12-08 22:23:16 -0500

    12/9/18

---
+++ Solutions/002/solution_I.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+        ListNode p1 = l1, p2 = l2;
+        int sum = 0;
+        
+        while (p1 != null || p2 != null) {
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            cur.next = new ListNode(sum % 10);
+            sum /= 10;
+            cur = cur.next;
+        }
+        if (sum == 1) {
+            cur.next = new ListNode(sum);
+        }
+        return dummy.next;
+    }
+}
+
+O(n);
+O(n);
