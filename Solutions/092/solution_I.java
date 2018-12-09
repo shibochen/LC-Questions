6598f247abcf83ffbe25ca04c56bce99d12d83b7
Shibo Chen <shibochenny@gmail.com>
2018-12-09 00:02:24 -0500

    12/8/18

--- Solutions/092/solution_I.java
+++ Solutions/092/solution_I.java
@@ -20,6 +20,16 @@ class Solution {
     }
 }
 
-
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+       
+line 8-11    dummy->1->2->3->4->5->NULL
+                    p  c  t
+line 13-18   T1       14:  temp = 3
+                      15:  2->4
+                      16:  3->2
+                      17:  1->3 
+                      总： 1->3->2->4  
+             ...
+Output:   Output: 1->4->3->2->5->NULL
 O(n)
 O(1)
