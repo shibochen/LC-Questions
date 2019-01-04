4963a10fa03311c58758efe8aac125a481395f7f
Shibo Chen <shibochenny@gmail.com>
2019-01-03 22:36:08 -0500

    1/3/19

---
+++ Solutions/094/solution_I.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        
+        while(cur != null || !stack.isEmpty()) {
+            while(cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+        return result;
+    }
+}
+
+
+O(n)
+O(n)
