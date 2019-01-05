381e1f36370a956446c5e67b66eb773b93133746
Shibo Chen <shibochenny@gmail.com>
2019-01-04 22:50:31 -0500

    1/4/19

---
+++ Solutions/107/solution_I.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+                list.add(cur.val);
+            }
+            result.add(0, list);
+        }
+        return result;
+    }
+}
+
+O(n)
+O(n)
