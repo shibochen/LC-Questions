e8082068e281237e53fab9191ef5a6eba5d6cb86
Shibo Chen <shibochenny@gmail.com>
2019-01-07 23:33:43 -0500

    1/7/19

---
+++ Solutions/383/solution_I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++;
+        }
+        
+        for (int j = 0; j < ransomNote.length(); j++) {
+            if (--count[ransomNote.charAt(j) - 'a'] < 0) return false;
+        }
+        
+        return true;
+    }
+}
+
+O(n)
+O(1)
