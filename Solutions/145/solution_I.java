01b0d0222f7e6d25b6e1210d6435c42647618682
Shibo Chen <shibochenny@gmail.com>
2019-01-15 23:33:36 -0500

    update solutions

--- Solutions/145/solution_I.java
+++ Solutions/145/solution_I.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         LinkedList<Integer> list = new LinkedList<>();
+//        List<Integer> list = new LinkedList<>();  这种写法不行，List没有addFirst 方法
         if(root == null) return list;
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
@@ -18,3 +19,5 @@ class Solution {
 
 O(n)
 O(n)
+    
+跟先序遍历的写法差不多，注意先加左边再加右边（line 12 - 13）
---
+++ Solutions/145/solution_II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(result, root);
+        return result;
+    }
+    public void helper(List<Integer> result, TreeNode root) {
+        if (root == null) return;
+        helper(result, root.left);
+        helper(result, root.right);
+        result.add(root.val);
+    } 
+}
