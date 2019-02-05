29a1bae6b91342b3ba25342dcfc74031f0c06cb1
Shibo Chen <shibochenny@gmail.com>
2019-02-04 23:46:12 -0500

    2/4/19

---
+++ Solutions/283/solution_I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        while (j < nums.length) {
+            nums[j++] = 0;
+        }
+    }
+}
+
+O(n)
+O(1)
