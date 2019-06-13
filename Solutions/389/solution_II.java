ce8e7ffdde099c718906a04a86323470d1e1368d
Shibo Chen <shibochenny@gmail.com>
2019-06-12 23:51:25 -0400

    delete old folder

--- Solutions/389/solution_II.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public char findTheDifference(String s, String t) {
-        int res = 0;
-        for (char c : (s + t).toCharArray())
-            res ^= c;
-        return (char) res;
-    }
-}
